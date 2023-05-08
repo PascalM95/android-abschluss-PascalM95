2c86141232b782aeb73e0dde4f88d159ff248d44
Pascal Mönch <pascal@tbmoench.de>
2023-05-08 13:37:54 +0200

    MatchTracker Pro (Pascal Mönch) 08.05.2023

---
+++ app/src/main/java/com/example/matchtrackerpro/ui/TableFragment.kt
@@ -0,0 +1,12 @@
+package com.example.matchtrackerpro.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.matchtrackerpro.R
+
+class TableFragment : Fragment() {
+
+}
