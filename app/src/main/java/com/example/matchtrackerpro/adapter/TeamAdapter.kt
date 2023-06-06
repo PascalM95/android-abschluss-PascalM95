c35cba01869993be638db60c33d8d98b4439e1a2
Pascal Mönch <pascal@tbmoench.de>
2023-06-06 14:00:27 +0200

    MatchTracker Pro (Pascal Mönch) 06.06.2023

---
+++ app/src/main/java/com/example/matchtrackerpro/adapter/TeamAdapter.kt
@@ -0,0 +1,53 @@
+package com.example.matchtrackerpro.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.core.net.toUri
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import coil.transform.RoundedCornersTransformation
+import com.example.matchtrackerpro.R
+import com.example.matchtrackerpro.SharedViewModel
+import com.example.matchtrackerpro.data.datamodels.Team
+import com.google.android.material.card.MaterialCardView
+
+class TeamAdapter (
+    private val dataset: List<Team>,
+    private val viewModel: SharedViewModel
+    ):RecyclerView.Adapter<TeamAdapter.ItemViewHolder>() {
+
+        class ItemViewHolder ( val view: View): RecyclerView.ViewHolder(view) {
+            val imgTeam: ImageView = view.findViewById(R.id.iv_team)
+            val cvTeam: MaterialCardView = view.findViewById(R.id.cv_team)
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+
+        val teamAdapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.team_item, parent, false)
+
+        return ItemViewHolder(teamAdapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val team = dataset[position]
+
+        val imgUri = team.img.toUri().buildUpon().scheme("https").build()
+
+        holder.imgTeam.load(imgUri) {
+            transformations(RoundedCornersTransformation(10f))
+        }
+
+        holder.cvTeam.setOnClickListener {
+            val navController = holder.view.findNavController()
+            navController.navigate(R.id.action_teamFragment_to_teamOrTableFragment)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return dataset.size
+    }
+}
